6372fb38b592c3a52d4905cf62503ba40dcdcfab
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-06-24 21:10:06 +0530

    Time: 96 ms (9.17%), Space: 38.5 MB (20.35%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cs
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cs
@@ -3,13 +3,13 @@ public class Solution {
         int n = gain.Length;
         int[] altitude = new int[n+1];
         altitude[0] = 0;
-        int max = altitude[0];
+        
         for(int i=1; i<=n; i++)
         {
             altitude[i] = gain[i-1] + altitude[i-1];
-            if(altitude[i]>max) max = altitude[i];
+            
         }
 
-        return max;
+        return altitude.Max();
     }
 }
