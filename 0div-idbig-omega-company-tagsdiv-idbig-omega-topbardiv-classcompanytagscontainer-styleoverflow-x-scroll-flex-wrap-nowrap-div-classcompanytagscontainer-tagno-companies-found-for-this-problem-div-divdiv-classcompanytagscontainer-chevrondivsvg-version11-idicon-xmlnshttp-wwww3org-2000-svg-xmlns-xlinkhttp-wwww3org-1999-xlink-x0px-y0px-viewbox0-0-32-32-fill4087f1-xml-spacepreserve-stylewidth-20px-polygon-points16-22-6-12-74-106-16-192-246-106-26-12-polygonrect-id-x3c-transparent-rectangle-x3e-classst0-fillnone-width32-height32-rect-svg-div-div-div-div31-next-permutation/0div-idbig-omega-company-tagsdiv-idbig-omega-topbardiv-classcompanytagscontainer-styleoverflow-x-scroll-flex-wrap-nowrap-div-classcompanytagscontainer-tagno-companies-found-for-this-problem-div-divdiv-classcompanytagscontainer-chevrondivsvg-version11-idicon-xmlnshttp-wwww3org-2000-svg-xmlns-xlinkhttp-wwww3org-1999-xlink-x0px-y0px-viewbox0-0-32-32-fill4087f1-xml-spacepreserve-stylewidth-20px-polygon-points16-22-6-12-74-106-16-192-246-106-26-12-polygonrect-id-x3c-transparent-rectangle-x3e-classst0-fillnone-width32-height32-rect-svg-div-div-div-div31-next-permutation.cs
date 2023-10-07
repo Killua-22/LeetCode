f84ace33c9e26bbeb09b306d0b2f211079483a16
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-10-07 19:06:22 +0530

    Time: 123 ms (82.54%), Space: 43.2 MB (94.76%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div31-next-permutation/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div31-next-permutation.cs
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div31-next-permutation/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div31-next-permutation.cs
@@ -3,8 +3,7 @@ public class Solution {
         int n = nums.Length;
         int x = -1;
         int y = -1;
-        
-        for(int i = n-2; i>=0; i--)
+        for(int i=n-2; i>=0; i--)
         {
             if(nums[i] < nums[i+1])
             {
@@ -15,25 +14,35 @@ public class Solution {
         
         if(x == -1)
         {
-           reverse(nums, 0, n);
+            reverse(nums, 0, n);
+            return;
         }
-        else
-        {
-            for(int i=n-1; i>=0; i--)
-            {
-                if(nums[i] > nums[x])
-                {
-                    y = i;
-                    break;
-                }
-            }
-            
-            swap(nums, x, y);
-            
-            reverse(nums, x+1, n);
-        }
-
         
+        for(int i=n-1; i>=0; i--)
+        {
+            if(nums[i] > nums[x])
+            {
+                y = i;
+                break;
+            }
+        }
+        
+        swap(nums, x, y);
+        reverse(nums, x+1, n);
+    
+    }
+    
+    public void reverse(int[] nums, int start, int n)
+    {
+        int i = start; 
+        int j = n-1;
+        while(i<j)
+        {
+            swap(nums, i, j);
+            i++;
+            j--;
+                
+        }
     }
     
     public void swap(int[] nums, int i, int j)
@@ -41,18 +50,6 @@ public class Solution {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-    }
-    
-    public void reverse(int[] nums, int start, int n)
-    {
-        int i = start;
-        int j = n-1;
-        while(i<j)
-        {
-            swap(nums, i, j);
-            i++;
-            j--;
-        }
     }
     
 }
