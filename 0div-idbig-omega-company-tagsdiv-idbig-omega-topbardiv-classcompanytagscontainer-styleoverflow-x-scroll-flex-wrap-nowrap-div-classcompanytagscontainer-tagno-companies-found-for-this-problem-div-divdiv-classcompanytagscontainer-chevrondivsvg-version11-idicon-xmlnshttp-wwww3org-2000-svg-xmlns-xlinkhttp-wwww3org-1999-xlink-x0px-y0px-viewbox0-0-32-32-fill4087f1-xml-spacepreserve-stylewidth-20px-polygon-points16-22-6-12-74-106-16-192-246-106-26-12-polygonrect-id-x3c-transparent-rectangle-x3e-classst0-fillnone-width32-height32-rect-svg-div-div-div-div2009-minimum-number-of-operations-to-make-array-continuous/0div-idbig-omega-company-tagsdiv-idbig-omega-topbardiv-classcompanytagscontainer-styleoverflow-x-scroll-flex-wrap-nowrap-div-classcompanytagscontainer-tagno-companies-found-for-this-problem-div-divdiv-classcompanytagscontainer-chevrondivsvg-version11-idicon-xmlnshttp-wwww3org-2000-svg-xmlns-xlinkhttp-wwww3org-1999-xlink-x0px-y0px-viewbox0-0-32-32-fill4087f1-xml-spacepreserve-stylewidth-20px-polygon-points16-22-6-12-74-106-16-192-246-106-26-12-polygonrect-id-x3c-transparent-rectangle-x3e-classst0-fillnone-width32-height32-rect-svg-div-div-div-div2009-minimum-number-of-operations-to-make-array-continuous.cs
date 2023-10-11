11836cfa819d6f71b1721bc7cc1d2d2fa3aea326
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-10-11 10:16:28 +0530

    Time: 222 ms (100.00%), Space: 61.3 MB (50.00%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous.cs
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous.cs
@@ -1,31 +1,30 @@
 public class Solution {
-
-    
     public int MinOperations(int[] nums) {
         int n = nums.Length;
         Array.Sort(nums);
         
-        List<int> list = new List<int>();
+        List<int> lt = new List<int>();
         
-        for (int i = 0; i < n; i++)
+        for(int i=0; i<n; i++)
         {
-            if (i > 0 && nums[i] == nums[i - 1])
+            if(i>0 && nums[i] == nums[i-1])
                 continue;
             
-            list.Add(nums[i]);
-        }
-        int len = list.Count;
-        int res = n;
-        int r = 0;
-        for(int l=0; l<len; l++)
-        {
-            
-            while(r<len && list[r] < list[l] + n)
-                r += 1;
-            int window = r - l;
-            res = Math.Min(res, n - window);
+            lt.Add(nums[i]);
         }
         
+        int len = lt.Count;
+        int r = 0;
+        int res = n;
+        for(int l=0; l<len; l++)
+        {
+            while(r<len && lt[r]<lt[l]+n)
+                r++;
+            int window = r-l;
+            
+            res = Math.Min(res, n - window);    
+                
+        }
         return res;
     }
 }
