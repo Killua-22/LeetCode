be4dd2f9ade069a08f6fd8a7ad1d0ed319d5afd1
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-10-12 11:05:39 +0530

    Time: 77 ms (44.49%), Space: 38.1 MB (66.93%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsalesforce-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div316-remove-duplicate-letters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsalesforce-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div316-remove-duplicate-letters.cs
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsalesforce-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div316-remove-duplicate-letters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsalesforce-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div316-remove-duplicate-letters.cs
@@ -1,66 +1,67 @@
 public class Solution {
     public string RemoveDuplicateLetters(string s) {
-//         int n = s.Length;
-//         bool[] visited = new bool[26];
-//         int[] frq = new int[26];
-//         Stack<char> st = new Stack<char>();
+        int n = s.Length;
+        bool[] visited = new bool[26];
+        int[] frq = new int[26];
+        Stack<char> st = new Stack<char>();
         
-//         for(int i=0; i<n; i++)
-//         {
-//             frq[s[i] - 'a']++;
-//         }
-        
-        
-//         for(int i=0; i<n; i++)
-//         {
-//             if(visited[s[i]-'a'])
-//             {
-//                 frq[s[i]-'a']--;
-//                 continue;
-//             }
-            
-//             while(st.Count!=0 && frq[s[i]-'a'] > 0 && s[i] < st.Peek())
-//             {
-//                 visited[st.Peek() - 'a'] = false;
-//                 st.Pop();
-//             }
-            
-//             st.Push(s[i]);
-//             visited[s[i]-'a'] = true;
-//             frq[s[i]-'a']--;
-                
-//         }
-        
-//         StringBuilder ans = new();
-        
-//         while(st.Count!=0)
-//         {
-//             ans.Insert(0, st.Pop());
-//         }
-        
-        
-//         string temp = ans.ToString();
-//         return temp;
-        Stack<char> stack = new Stack<char>();
-        HashSet<char> seen = new HashSet<char>();
-        Dictionary<char, int> lastOcc = new Dictionary<char, int>();
-        for (int i = 0; i < s.Length; i++) {
-            lastOcc[s[i]] = i;
+        for(int i=0; i<n; i++)
+        {
+            frq[s[i] - 'a']++;
         }
         
-        for (int i = 0; i < s.Length; i++) {
-            char c = s[i];
-            if (!seen.Contains(c)) {
-                while (stack.Count > 0 && c < stack.Peek() && i < lastOcc[stack.Peek()]) {
-                    seen.Remove(stack.Pop());
-                }
-                seen.Add(c);
-                stack.Push(c);
+        
+        for(int i=0; i<n; i++)
+        {
+            if(visited[s[i]-'a'])
+            {
+                frq[s[i]-'a']--;
+                continue;
             }
+            
+            while(st.Count!=0 && frq[st.Peek()-'a'] > 0 && s[i] < st.Peek())
+            {
+                visited[st.Peek() - 'a'] = false;
+                st.Pop();
+            }
+            
+            st.Push(s[i]);
+            visited[s[i]-'a'] = true;
+            frq[s[i]-'a']--;
+                
         }
         
-        char[] result = stack.ToArray();
-        Array.Reverse(result);
-        return new string(result);
+        StringBuilder ans = new();
+        
+        while(st.Count!=0)
+        {
+            ans.Insert(0, st.Pop());
+        }
+        
+        
+        string temp = ans.ToString();
+        return temp;
+        
+//         Stack<char> stack = new Stack<char>();
+//         HashSet<char> seen = new HashSet<char>();
+//         Dictionary<char, int> lastOcc = new Dictionary<char, int>();
+//         for (int i = 0; i < s.Length; i++) {
+//             lastOcc[s[i]] = i;
+//         }
+        
+//         for (int i = 0; i < s.Length; i++) {
+//             char c = s[i];
+//             if (!seen.Contains(c)) {
+//                 while (stack.Count > 0 && c < stack.Peek() && i < lastOcc[stack.Peek()]) {
+//                     seen.Remove(stack.Pop());
+//                 }
+//                 seen.Add(c);
+//                 stack.Push(c);
+//             }
+//         }
+        
+//         char[] result = stack.ToArray();
+//         Array.Reverse(result);
+//         return new string(result);
     }
 }
