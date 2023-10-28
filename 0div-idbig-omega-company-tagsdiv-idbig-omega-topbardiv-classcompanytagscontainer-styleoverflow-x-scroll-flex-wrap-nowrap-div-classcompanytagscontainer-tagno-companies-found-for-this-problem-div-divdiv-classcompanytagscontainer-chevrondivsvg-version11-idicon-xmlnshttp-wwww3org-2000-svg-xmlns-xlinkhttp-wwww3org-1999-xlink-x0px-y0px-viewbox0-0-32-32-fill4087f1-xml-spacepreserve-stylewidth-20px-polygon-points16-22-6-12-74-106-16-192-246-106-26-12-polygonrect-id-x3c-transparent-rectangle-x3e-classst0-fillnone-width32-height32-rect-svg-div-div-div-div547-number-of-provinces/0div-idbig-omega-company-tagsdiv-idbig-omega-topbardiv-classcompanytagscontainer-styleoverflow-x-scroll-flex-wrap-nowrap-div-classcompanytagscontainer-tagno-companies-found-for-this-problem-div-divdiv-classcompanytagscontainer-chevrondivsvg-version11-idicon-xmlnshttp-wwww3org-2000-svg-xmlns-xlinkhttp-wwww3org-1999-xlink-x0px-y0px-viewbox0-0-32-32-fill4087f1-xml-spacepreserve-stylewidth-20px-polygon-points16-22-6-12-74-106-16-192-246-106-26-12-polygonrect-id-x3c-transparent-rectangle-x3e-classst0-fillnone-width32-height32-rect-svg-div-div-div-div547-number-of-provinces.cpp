db3fa1210a592b0e9f13409fab9f860b6a6ec199
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-10-28 17:14:15 +0530

    Time: 27 ms (25.20%), Space: 17 MB (30.99%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div547-number-of-provinces/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div547-number-of-provinces.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    
+//     void dfs(int node, vector<int> adjLs[], vector<bool> vis){
+//         vis[node] = true;
+        
+//         for(auto it: adjLs[node])
+//         {
+//             if(!vis[it])
+//                 dfs(it, adjLs, vis);
+//         }
+//     }
+    
+    void dfs(int node, vector<int> adjLs[], int vis[])
+    {
+        vis[node] = 1;
+        for(auto it: adjLs[node])
+        {
+            if(!vis[it])
+                dfs(it, adjLs, vis);
+        }
+    }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+//         int V = isConnected.size();
+//         vector<int> adjLs[V];
+        
+//         for(int i=0; i<V; i++)
+//         {
+//             for(int j=0; j<V; j++)
+//             {
+//                 if(isConnected[i][j] == 1 && i != j)
+//                 {
+//                     adjLs[i].push_back(j);
+//                     adjLs[j].push_back(i);
+//                 }
+//             }
+//         }
+        
+//         vector<bool> vis(V);
+        
+//         int count = 0;
+//         for(int i=0; i<V; i++)
+//         {
+//             if(!vis[i]){   
+//                 dfs(i, adjLs, vis);
+//                 count++;
+//             }
+//         }
+        
+//         return count;
+        int V = isConnected.size();
+        vector<int> adjLs[V];
+        int count = 0;
+        for(int i=0; i<V; i++)
+        {
+            for(int j=0; j<V; j++)
+            {
+                if(isConnected[i][j] == 1 && i != j)
+                {
+                    adjLs[i].push_back(j);
+                    adjLs[j].push_back(i);
+                }
+            }
+        }
+        int vis[201] = {0};
+        for(int i=0; i<V; i++)
+        {
+            if(!vis[i]){
+                count++;
+                dfs(i, adjLs, vis);
+            }
+                
+        }
+        return count;
+    }
+};
