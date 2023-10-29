4469d2b3b7e45a8a7243682f0d26aabe21434b9a
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-10-29 10:56:33 +0530

    Time: 607 ms (5.15%), Space: 152.6 MB (5.11%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divhotstar-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div841-keys-and-rooms/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divhotstar-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div841-keys-and-rooms.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    void dfs(int node, vector<vector<int>> rooms, bool vis[])
+    {
+        vis[node] = true;
+        for(auto it: rooms[node])
+        {
+            if(!vis[it])
+                dfs(it, rooms, vis);
+        }
+    }
+    
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        bool vis[n];
+        
+        for(int i=0; i<n; i++)
+        {
+            vis[i] = false;
+        }
+        
+        dfs(0, rooms, vis);
+        
+        for(int i=0; i<n; i++){
+            if(vis[i] == false)
+                return false;
+        }
+        return true;
+    }
+};
