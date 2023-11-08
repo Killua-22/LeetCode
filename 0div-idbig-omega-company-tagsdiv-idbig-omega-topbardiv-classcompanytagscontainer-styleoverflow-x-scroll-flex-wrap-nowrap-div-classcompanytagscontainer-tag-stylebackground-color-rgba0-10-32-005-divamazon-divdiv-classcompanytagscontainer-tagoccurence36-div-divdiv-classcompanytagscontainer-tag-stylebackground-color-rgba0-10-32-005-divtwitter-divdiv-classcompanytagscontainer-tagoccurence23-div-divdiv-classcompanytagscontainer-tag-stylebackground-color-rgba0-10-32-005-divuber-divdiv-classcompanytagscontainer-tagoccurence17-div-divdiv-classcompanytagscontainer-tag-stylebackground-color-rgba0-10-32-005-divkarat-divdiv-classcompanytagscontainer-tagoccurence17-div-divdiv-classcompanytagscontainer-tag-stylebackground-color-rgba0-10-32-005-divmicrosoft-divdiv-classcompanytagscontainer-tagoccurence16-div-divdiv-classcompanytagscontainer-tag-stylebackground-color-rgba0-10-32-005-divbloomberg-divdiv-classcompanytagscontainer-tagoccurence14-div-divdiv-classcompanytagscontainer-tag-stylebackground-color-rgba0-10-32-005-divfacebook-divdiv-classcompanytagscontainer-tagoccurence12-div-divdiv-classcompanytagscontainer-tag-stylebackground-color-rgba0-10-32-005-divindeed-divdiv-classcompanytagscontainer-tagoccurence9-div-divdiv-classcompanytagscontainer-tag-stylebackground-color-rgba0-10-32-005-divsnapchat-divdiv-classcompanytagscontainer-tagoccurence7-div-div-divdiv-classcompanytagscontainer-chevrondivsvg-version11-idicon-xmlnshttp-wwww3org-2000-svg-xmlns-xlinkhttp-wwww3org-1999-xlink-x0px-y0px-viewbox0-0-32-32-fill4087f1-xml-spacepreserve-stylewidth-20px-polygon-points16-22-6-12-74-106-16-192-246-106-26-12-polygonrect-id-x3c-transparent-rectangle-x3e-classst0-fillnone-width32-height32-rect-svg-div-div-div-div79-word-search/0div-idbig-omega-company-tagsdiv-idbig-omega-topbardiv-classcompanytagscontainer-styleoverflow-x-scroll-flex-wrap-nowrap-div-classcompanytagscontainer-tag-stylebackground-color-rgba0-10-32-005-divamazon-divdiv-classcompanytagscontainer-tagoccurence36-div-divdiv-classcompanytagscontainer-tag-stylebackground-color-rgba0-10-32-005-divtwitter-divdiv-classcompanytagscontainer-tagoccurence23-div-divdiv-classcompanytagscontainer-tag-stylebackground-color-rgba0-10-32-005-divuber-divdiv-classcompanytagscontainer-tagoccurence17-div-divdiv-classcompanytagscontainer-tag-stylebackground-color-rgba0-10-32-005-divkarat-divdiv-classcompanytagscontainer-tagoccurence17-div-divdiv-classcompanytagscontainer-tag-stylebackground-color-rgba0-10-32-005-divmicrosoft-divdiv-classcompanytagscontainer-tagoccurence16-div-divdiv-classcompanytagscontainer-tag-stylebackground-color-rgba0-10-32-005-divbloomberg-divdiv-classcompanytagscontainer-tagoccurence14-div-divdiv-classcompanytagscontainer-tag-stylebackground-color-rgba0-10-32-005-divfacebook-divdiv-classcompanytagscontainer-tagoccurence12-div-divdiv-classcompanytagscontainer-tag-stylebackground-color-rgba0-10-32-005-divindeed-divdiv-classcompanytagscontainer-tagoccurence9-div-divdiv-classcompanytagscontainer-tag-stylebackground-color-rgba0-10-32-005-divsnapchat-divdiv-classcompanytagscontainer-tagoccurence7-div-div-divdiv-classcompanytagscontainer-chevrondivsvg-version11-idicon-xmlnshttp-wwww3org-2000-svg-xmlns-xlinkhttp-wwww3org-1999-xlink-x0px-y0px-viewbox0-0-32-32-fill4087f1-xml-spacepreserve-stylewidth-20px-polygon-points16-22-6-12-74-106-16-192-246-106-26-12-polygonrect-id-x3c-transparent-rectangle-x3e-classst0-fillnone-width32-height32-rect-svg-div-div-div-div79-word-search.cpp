7daffc5af5d1c246f1c8c406911b0c50cb811914
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-11-08 16:22:58 +0530

    Time: 2276 ms (5.00%), Space: 8.6 MB (21.83%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence36-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitter-divdiv-classcompanytagscontainer-tagoccurence23-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence17-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence17-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence16-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence14-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence12-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divindeed-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsnapchat-divdiv-classcompanytagscontainer-tagoccurence7-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div79-word-search/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence36-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitter-divdiv-classcompanytagscontainer-tagoccurence23-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence17-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence17-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence16-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence14-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence12-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divindeed-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsnapchat-divdiv-classcompanytagscontainer-tagoccurence7-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div79-word-search.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    
+    vector<int> dx = {0, 0, -1, 1};
+    vector<int> dy = {1, -1, 0, 0};
+    
+    bool validIndex(int m, int n, int r, int c)
+    {
+        return r<m && r>=0 && c<n && c>=0; 
+    }
+    
+    bool backtracking(vector<vector<char>> &board, vector<vector<bool>> &vis, string target, int m, int n, int i, int j, string s) {
+        if(s == target)
+            return true;
+        if(s.length() > target.length())
+            return false;
+        if(!validIndex(m, n, i, j))
+            return false;
+        if(vis[i][j])
+            return false;
+        
+        vis[i][j] = true;
+        
+        bool can = false;
+        
+        for(int k=0; k<4; k++) {
+            if(can)
+                return true;
+            if(board[i][j] == target[s.length()])
+                can = backtracking(board, vis, target, m, n, i + dx[k], j + dy[k], s + board[i][j]);
+        }
+        
+        vis[i][j] = false;
+        return can;
+            
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        int rows = board.size();
+        int cols = board[0].size();
+        
+        
+        vector<vector<bool>> vis(rows, vector<bool>(cols));
+        
+        for(int r=0; r<rows; r++)
+        {
+            for(int c=0; c<cols; c++)
+            {
+                if (backtracking(board, vis, word, rows, cols, r, c, ""))
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    
+};
