d1cfd1cc73490b200dc3a79718442fd646ea6267
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-11-26 14:22:32 +0530

    Time: 4 ms (92.80%), Space: 12.6 MB (79.11%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoldman-sachs-divdiv-classcompanytagscontainer-tagoccurence11-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div740-delete-and-earn/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoldman-sachs-divdiv-classcompanytagscontainer-tagoccurence11-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div740-delete-and-earn.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoldman-sachs-divdiv-classcompanytagscontainer-tagoccurence11-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div740-delete-and-earn/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoldman-sachs-divdiv-classcompanytagscontainer-tagoccurence11-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div740-delete-and-earn.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
     int t[20001]; // declaring 1 d array, to store some already computed results
-    int solve(vector<int> &arr, int i)
-    {
-        if(i >= arr.size()) // if i is greater than size of array
-        {
-            return 0; // then simply returnn zero
+    
+    int solve(vector<int> &arr, int i) {
+        
+        if(i >= arr.size()){
+            return 0;
         }
         
-         // if result is already computed, then return from here
-        // this one line which we say to add
-        if(t[i] != -1)
-        {
+        if(t[i] != -1) {
             return t[i];
         }
         
-        // current 'i' on which we are standing
-        int currValue = arr[i];  // current value
-        int currSum = arr[i]; // intial make sum as same as value
-        int index = i + 1; // index to take elemets, so  i + 1
+        int currVal = arr[i];
+        int currSum = arr[i];
+        int index = i+1;
         
-        // while it is the same as the current value, include in our sum
-        while(index < arr.size() && arr[index] == currValue)
-        {
+        while(index < arr.size() && arr[index] == currVal){
             currSum += arr[i];
             index++;
         }
         
-        // Now, we have to skip all the elements, whose value is equal to
-        // currValue + 1
-        while(index < arr.size() && arr[index] == currValue + 1)
-        {
+        while(index < arr.size() && arr[index] == currVal + 1) {
             index++;
         }
         
-        //And lastly, we have two choices-
-        //whether to include the sum of this current element in our answer
-        // or not include the sum of current element in our answer
-        // so we explore all possibility and take maximum of them
+        return t[i] = max(currSum + solve(arr, index), solve(arr, i+1));
         
-        return t[i] = max(currSum + solve(arr, index), solve(arr, i + 1));
-        
-        // If we decide to take the curr element in our answer, then upto the elemet we skip the next value, we paas that index
-        // but if decided no to make this vurrent element then simply paas
-        // i + 1
     }
+    
     int deleteAndEarn(vector<int>& arr) {
         int n = arr.size(); // take the size of the array
         
@@ -58,4 +42,50 @@ public:
         //                ↑
         //                we start from zero index
     }
+    
+//     int solve(vector<int> &arr, int i)
+//     {
+//         if(i >= arr.size()) // if i is greater than size of array
+//         {
+//             return 0; // then simply returnn zero
+//         }
+        
+//          // if result is already computed, then return from here
+//         // this one line which we say to add
+//         if(t[i] != -1)
+//         {
+//             return t[i];
+//         }
+        
+//         // current 'i' on which we are standing
+//         int currValue = arr[i];  // current value
+//         int currSum = arr[i]; // intial make sum as same as value
+//         int index = i + 1; // index to take elemets, so  i + 1
+        
+//         // while it is the same as the current value, include in our sum
+//         while(index < arr.size() && arr[index] == currValue)
+//         {
+//             currSum += arr[i];
+//             index++;
+//         }
+        
+//         // Now, we have to skip all the elements, whose value is equal to
+//         // currValue + 1
+        
+//         while(index < arr.size() && arr[index] == currValue + 1)
+//         {
+//             index++;
+//         }
+        
+//         //And lastly, we have two choices-
+//         //whether to include the sum of this current element in our answer
+//         // or not include the sum of current element in our answer
+//         // so we explore all possibility and take maximum of them
+        
+//         return t[i] = max(currSum + solve(arr, index), solve(arr, i + 1));
+        
+//         // If we decide to take the curr element in our answer, then upto the elemet we skip the next value, we paas that index
+//         // but if decided no to make this vurrent element then simply paas
+//         // i + 1
+//     }
 };
