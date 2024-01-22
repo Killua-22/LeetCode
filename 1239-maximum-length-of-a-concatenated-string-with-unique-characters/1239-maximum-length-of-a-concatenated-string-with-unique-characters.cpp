f06e4d10d16bd70e330af29e3c6bb699b3759714
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2024-01-22 13:03:20 +0530

    Time: 47 ms (59.33%), Space: 47.8 MB (51.09%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    
+    bool isvalid(vector<bool> &ispicked, string &curr) {
+        
+        vector<bool> repeatingstring(26, false);
+        
+        for(int i=0; i<curr.size(); i++) {
+            if(repeatingstring[curr[i] - 'a'])
+                return false;
+            repeatingstring[curr[i] - 'a'] = true;
+        }
+        
+        for(int i=0; i<curr.size(); i++) {
+            if(ispicked[curr[i] - 'a'])
+                return false;
+        }
+        
+        return true;
+        
+    }
+    
+    int helper(int index, vector<string>& arr, vector<bool>& ispicked, int len) {
+        if(index == arr.size())
+            return len;
+        
+        string curr = arr[index];
+        
+        if(!isvalid(ispicked, curr)) {
+            return helper(index+1, arr, ispicked, len);
+        }
+        else {
+            
+            for(int i=0; i<curr.size(); i++)
+                ispicked[curr[i]-'a'] = true;
+            
+            len += curr.size();
+            
+            int pick = helper(index+1, arr, ispicked, len);
+            
+            for(int i=0; i<curr.size(); i++)
+                ispicked[curr[i]-'a'] = false;
+            
+            len -= curr.size();
+            
+            int notpick = helper(index+1, arr, ispicked, len);
+            
+            return max(pick, notpick);
+        }
+    }
+    
+    int maxLength(vector<string>& arr) {
+        vector<bool> ispicked(26, false);
+        return helper(0, arr, ispicked, 0);
+    }
+    
+    
+};
