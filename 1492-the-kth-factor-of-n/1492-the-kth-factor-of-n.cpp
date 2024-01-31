5791e00924a1db3b99e05131c1309e85676d4afa
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2024-01-31 20:19:50 +0530

    Time: 4 ms (5.38%), Space: 7.3 MB (9.51%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -5,15 +5,12 @@ public:
         vector<int> factors;
         
         for(int i=1; i<=n; i++) {
-            if(n % i == 0){
-                factors.push_back(i);
-                cout<<i<<endl;
-            }
-                
-            
+            if(n%i==0) factors.push_back(i);
         }
         
-        return k <= factors.size() ? factors[k-1] : -1;
+        if(factors.size() < k) return -1;
+        
+        return factors[k-1];
         
     }
 };
