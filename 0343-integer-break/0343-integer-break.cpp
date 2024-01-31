980f6744f7b98b27f47d2e728af1a39fd5d1eb0b
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2024-01-31 19:58:03 +0530

    Time: 2 ms (47.62%), Space: 7 MB (21.02%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int integerBreak(int n) {
         
-        int res=0;
+        int res = 0;
+        
         for(int k=2; k<=n; k++) {
-            int repeatingnums = n / k;
-            int addtorepeatingnums = n % k;
+            int repeatingnumbers = n / k;
+            int toadd = n % k;
+            
             int temp = 1;
             
             for(int i=0; i<k; i++) {
-                if(addtorepeatingnums) {
-                    temp *= (repeatingnums + 1);
-                    addtorepeatingnums--;
+                if(toadd) {
+                    temp *= (repeatingnumbers+1);
+                    toadd--;
                 }else {
-                    temp *= repeatingnums;
-                }  
-                
+                    temp *= repeatingnumbers;
+                }
             }
-            res = max(temp, res);
+            
+            res = max(res, temp);
         }
         
         return res;
-        
     }
 };
