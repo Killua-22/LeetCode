a1d02a40d17e395af9c55cf4fddd40c75c36ec37
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2024-02-13 17:14:58 +0530

    Time: 4 ms (21.80%), Space: 8.5 MB (19.07%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    void helper(int i, int k, int n, vector<int>& lst, vector<vector<int>>& res) {
+        
+        if(n == 0 && lst.size() == k) {
+            res.push_back(lst);
+            return;
+        }
+        
+        // if(lst.size() == k) 
+        // { 
+        //     return;
+        // }
+        
+        if(n < 0)
+            return;
+        if(i > 9)
+            return;
+        
+        lst.push_back(i);
+        
+        helper(i+1, k, n-i, lst, res);
+        
+        lst.pop_back();
+        
+        helper(i+1, k, n, lst, res);
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        
+        vector<vector<int>> res;
+        
+        vector<int> lst;
+        
+        helper(1, k, n, lst, res);
+        
+        return res;
+        
+    }
+};
