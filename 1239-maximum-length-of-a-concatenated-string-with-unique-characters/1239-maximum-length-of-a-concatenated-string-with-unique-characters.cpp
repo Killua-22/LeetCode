6a08756b998e8372933d1ce6baacbfa4dc1f5ac5
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2024-02-14 12:42:11 +0530

    Time: 45 ms (53.47%), Space: 48.1 MB (47.13%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     
-    bool isvalid(vector<bool> &ispicked, string &curr) {
+    bool isvalid(vector<bool>& ispicked, string s) {
         
-        vector<bool> repeatingstring(26, false);
+        vector<bool> repeat(26, false);
         
-        for(int i=0; i<curr.size(); i++) {
-            if(repeatingstring[curr[i] - 'a'])
+        for(int i=0; i<s.size(); i++) {
+            if(repeat[s[i] - 'a'])
                 return false;
-            repeatingstring[curr[i] - 'a'] = true;
+            repeat[s[i] - 'a'] = true;
         }
         
-        for(int i=0; i<curr.size(); i++) {
-            if(ispicked[curr[i] - 'a'])
+        for(int i=0; i<s.size(); i++) {
+            if(ispicked[s[i]-'a'])
                 return false;
         }
         
         return true;
-        
     }
     
     int helper(int index, vector<string>& arr, vector<bool>& ispicked, int len) {
+        
         if(index == arr.size())
             return len;
         
@@ -31,28 +31,32 @@ public:
         }
         else {
             
-            for(int i=0; i<curr.size(); i++)
-                ispicked[curr[i]-'a'] = true;
+            for(int i=0; i<curr.size(); i++) {
+                ispicked[curr[i] - 'a'] = true;
+            }
             
             len += curr.size();
             
             int pick = helper(index+1, arr, ispicked, len);
             
-            for(int i=0; i<curr.size(); i++)
-                ispicked[curr[i]-'a'] = false;
+            for(int i=0; i<curr.size(); i++) {
+                ispicked[curr[i] - 'a'] = false;
+            }
             
             len -= curr.size();
             
             int notpick = helper(index+1, arr, ispicked, len);
             
+            
             return max(pick, notpick);
         }
+
+        
     }
     
     int maxLength(vector<string>& arr) {
         vector<bool> ispicked(26, false);
         return helper(0, arr, ispicked, 0);
+        
     }
-    
-    
 };
