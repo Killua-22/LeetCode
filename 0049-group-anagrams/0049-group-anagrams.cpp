6ce071c1bbeeb21db9df2c8595f222c727e892c4
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2024-02-16 16:48:23 +0530

    Time: 48 ms (13.51%), Space: 33.1 MB (6.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     
-    vector<int> freq(string& s){
-        vector<int> alphabet(26, 0);
-        for (char c: s){
-            alphabet[c-'a']++;
-        }
-        return alphabet;
-    }
     
-    map<vector<int>, vector<int>> freq2idx;
-    
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
        
         int n=strs.size();
+        
+        map<vector<int>, vector<int>> freq2idx;
+        
         for(int i=0; i<n; i++){
-            vector<int> f=freq(strs[i]);
-            if (freq2idx.count(f)==0) freq2idx[f]={i};
-            else freq2idx[f].push_back(i);
+            
+            vector<int> cnt(26, 0);
+            
+            for(auto c: strs[i]) {
+                cnt[c-'a']++;
+            }
+            
+            if (freq2idx.count(cnt)==0) freq2idx[cnt]={i};
+            
+            else freq2idx[cnt].push_back(i);
         }
         vector<vector<string>> ans(freq2idx.size());
         int i=0; 
