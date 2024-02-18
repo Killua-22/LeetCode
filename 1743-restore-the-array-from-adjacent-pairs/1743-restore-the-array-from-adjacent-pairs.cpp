d30c71a494d6e633995e8b5051c9a16772f6442b
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2024-02-18 16:26:46 +0530

    Time: 1605 ms (5.28%), Space: 151.7 MB (20.93%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
-    
-    map<int, vector<int>> freq;
-    
-    void dfs(int root, vector<int>& ans, int prev) {
-        ans.push_back(root);
+    unordered_map<int, vector<int>> freq;
+    void dfs(int root, vector<int>& res, int prev) {
         
-        for(int i: freq[root]) {
-            if(i == prev) continue;
-            dfs(i, ans, root);
+        res.push_back(root);
+        
+        for(int c: freq[root]) {
+            if(c == prev) continue;
+            dfs(c, res, root);
         }
+        
     }
     
     vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
         
         vector<int> res;
-        int n = adjacentPairs.size();
         
         
-        
-        for(int i=0; i<n; i++) {
-            int x = adjacentPairs[i][0];
-            int y = adjacentPairs[i][1];
+        for(auto c: adjacentPairs) {
+            
+            int x = c[0];
+            int y = c[1];
+            
             if(freq.count(x)) freq[x].push_back(y);
             else freq[x] = {y};
-            
             if(freq.count(y)) freq[y].push_back(x);
             else freq[y] = {x};
             
         }
         
-        int first;
+        int root;
         
         for(auto x: freq) {
-            if(x.second.size() == 1){
-                first = x.first;
-                break;
-            }
-                
+            if(x.second.size() == 1)
+                root = x.first;
         }
-
         
-        dfs(first, res, INT_MAX);
+        
+        dfs(root, res, INT_MAX);
         
         return res;
     }
