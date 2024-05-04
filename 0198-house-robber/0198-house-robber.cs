ac331053b6a0f5b72ff6240f923578e740d94516
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2024-05-04 16:40:12 +0530

    Time: 57 ms (62.94%), Space: 40.4 MB (11.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.cs
+++ 0198-house-robber/0198-house-robber.cs
@@ -1,20 +1,34 @@
 public class Solution {
-    int[] memo;
-    public int Rob(int[] nums) {
-        memo = new int[nums.Length+1];
-        Array.Fill(memo, -1);
-        return rob(nums, nums.Length-1);
-    }
+//     int[] memo;
+//     public int Rob(int[] nums) {
+//         memo = new int[nums.Length+1];
+//         Array.Fill(memo, -1);
+//         return rob(nums, nums.Length-1);
+//     }
     
-    public int rob(int[] nums, int length) {
-        if(length < 0)
-            return 0;
+//     public int rob(int[] nums, int length) {
+//         if(length < 0)
+//             return 0;
         
-        if(memo[length] >= 0)
-            return memo[length];
+//         if(memo[length] >= 0)
+//             return memo[length];
         
-        int result = Math.Max(rob(nums, length-2) + nums[length], rob(nums, length-1));
-        memo[length] = result;
-        return result;
+//         int result = Math.Max(rob(nums, length-2) + nums[length], rob(nums, length-1));
+//         memo[length] = result;
+//         return result;
+//     }
+    
+    public int Rob(int[] nums) {
+        int[] dp = new int[nums.Length+1];
+        
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for(int i=1; i<nums.Length; i++) {
+            int val = nums[i];
+            dp[i+1] = Math.Max(dp[i], dp[i-1]+val);
+        }
+        
+        return dp[nums.Length];
     }
 }
