5f5cde7b16c11100a1cbbb0c911ba3936876f545
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2024-10-04 00:06:55 +0530

    Time: 0 ms (100%), Space: 8.7 MB (63.34%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
+
     bool isInterleave(string s1, string s2, string s3) {
         int i1 = s1.size();
         int i2 = s2.size();
         int i3 = s3.size();
-
-        if(i1+i2 != i3) 
+        if(i1 + i2 != i3)
             return false;
-        
+
         vector<vector<bool>> dp(i1+1, vector<bool>(i2+1));
+
         dp[0][0] = true;
 
         for(int i=1; i<=i1; i++) {
-            dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1];
+            dp[i][0] = dp[i-1][0] && s1[i-1] == s3[i-1];
         }
 
-        for(int j=1; j<=i2; j++) 
-        {
-            dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1];
+        for(int j=1; j<=i2; j++) {
+            dp[0][j] = dp[0][j-1] && s2[j-1] == s3[j-1];
         }
 
-        for(int i=1; i<=i1; i++) 
-        {
-            for(int j=1; j<=i2; j++) 
-            {
+        for(int i=1; i<=i1; i++) {
+            for(int j=1; j<=i2; j++) {
                 dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1]);
             }
         }
